a36da7fef83955109e5b5a2aa665e89720a9c4da
翟桐 <zt13936@ly.com>
2017-12-22 10:33:09 +0800

    目录结构修改

---
+++ ZTTextField-Swift/ZTTextField-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZTTextField-Swift
+//
+//  Created by zhaitong on 2017/12/22.
+//  Copyright © 2017年 zhaitong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZTTextField-Swift/ZTTextField-Swift/ZTTextField/ZTTextField.swift
@@ -0,0 +1,21 @@
+//
+//  ZTTextField.swift
+//  ZTTextField-Swift
+//
+//  Created by zhaitong on 2017/12/22.
+//  Copyright © 2017年 zhaitong. All rights reserved.
+//
+
+import UIKit
+
+class ZTTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
